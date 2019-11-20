d818551237a95671d578619cdfe73c69520861a1
Oscar Lennox-Hilton (s5063022) <s5063022@bournemouth.ac.uk>
2019-11-20 13:02:54 +0000

    More colourful!

--- src/main.cpp
+++ src/main.cpp
@@ -20,6 +20,25 @@ int main(int argc, char* argv[])
 	init(&window, &renderer);
 	std::cout << "START" << std::endl;
 
+	SDL_SetRenderDrawColor(renderer, 0, 0, 255, 255);
+	SDL_RenderClear(renderer);
+
+	int posX = 0;
+	int posY = 0;
+	for (float j = 0.0f; j < 1.0f; j += 0.001f)
+	{
+		for (float i = 0.0f; i < 1.0f; i += 0.001f)
+		{
+			int r = static_cast<int>(255 - (256 * j));
+			int g = static_cast<int>(255 * i);
+			int b = static_cast<int>(256 * j);
+			SDL_SetRenderDrawColor(renderer, r, g, b, 255);
+			SDL_RenderDrawPoint(renderer, posX, posY);
+			posX = static_cast<int>(640 * i);
+		}
+		posY = static_cast<int>(480 * j);
+	}
+
 	SDL_Event event;
 	bool quit = false;
 	while (!quit)
@@ -31,9 +50,7 @@ int main(int argc, char* argv[])
 			case SDL_QUIT: { quit = true; break; }
 			}
 		}
-
-		SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
-		SDL_RenderClear(renderer);
+		
 		SDL_RenderPresent(renderer);
 	}
 
